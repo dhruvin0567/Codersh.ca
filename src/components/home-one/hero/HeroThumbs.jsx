4706e05af4bb2d15eabc2ef92818fc94da944063
user.email <developer3.codersh@gmail.com>
2025-08-19 13:07:48 +0530

    Clouldinary images cdn impliment in a hero section

--- src/components/home-one/hero/HeroThumbs.jsx
+++ src/components/home-one/hero/HeroThumbs.jsx
@@ -1,56 +1,63 @@
 // import { useState, useEffect } from 'react';
-import HeroThumbImg1 from "/images/homepage/hero/Techless.webp";
-import HeroThumbImg2 from "/images/homepage/hero/Cohort.webp";
-import HeroThumbImg3 from "/images/homepage/hero/Kinvara.webp";
+// import HeroThumbImg1 from "/images/homepage/hero/Techless.webp";
+// import HeroThumbImg2 from "/images/homepage/hero/Cohort.webp";
+// import HeroThumbImg3 from "/images/homepage/hero/Kinvara.webp";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from 'swiper/modules';
+import { Autoplay } from "swiper/modules";
 
 function HeroThumbs() {
-
-
-	return (
-		<div className="aximo-hero-thumb jos" data-jos_animation="fade-right" >
-			<Swiper
-				spaceBetween={30} // Space between slides
-				slidesPerView={1} // Number of slides visible at once
-				loop={true} // Infinite loop of the slides
-				autoplay={{
-					delay: 2500,
-					disableOnInteraction: false,
-				}} // Autoplay settings
-				speed={1000}  // Transition speed in ms (1000ms = 1 second)
-				modules={[Autoplay]} // Adding the Autoplay module
-				breakpoints={{
-					// When window width is >= 320px
-					320: {
-						slidesPerView: 1, // 1 slide per view
-					},
-					// When window width is >= 480px
-					480: {
-						slidesPerView: 1, // 2 slides per view
-					},
-					// When window width is >= 768px
-					768: {
-						slidesPerView: 1 // 3 slides per view
-					},
-					// When window width is >= 1024px
-					1024: {
-						slidesPerView: 1, // 4 slides per view
-					},
-				}}
-			>
-				<SwiperSlide>
-					<img src={HeroThumbImg1} alt="HeroImg" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={HeroThumbImg2} alt="HeroImg" />
-				</SwiperSlide>
-				<SwiperSlide>
-					<img src={HeroThumbImg3} alt="HeroImg" />
-				</SwiperSlide>
-			</Swiper>
-		</div>
-	);
+  return (
+    <div className="aximo-hero-thumb jos" data-jos_animation="fade-right">
+      <Swiper
+        spaceBetween={30} // Space between slides
+        slidesPerView={1} // Number of slides visible at once
+        loop={true} // Infinite loop of the slides
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }} // Autoplay settings
+        speed={1000} // Transition speed in ms (1000ms = 1 second)
+        modules={[Autoplay]} // Adding the Autoplay module
+        breakpoints={{
+          // When window width is >= 320px
+          320: {
+            slidesPerView: 1, // 1 slide per view
+          },
+          // When window width is >= 480px
+          480: {
+            slidesPerView: 1, // 2 slides per view
+          },
+          // When window width is >= 768px
+          768: {
+            slidesPerView: 1, // 3 slides per view
+          },
+          // When window width is >= 1024px
+          1024: {
+            slidesPerView: 1, // 4 slides per view
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/dftnq7fna/image/upload/v1755587527/Kinvara_mudijz.webp"
+            alt="HeroImg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/dftnq7fna/image/upload/v1755587527/Techless_f1vg5r.webp"
+            alt="HeroImg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://res.cloudinary.com/dftnq7fna/image/upload/v1755587526/Cohort_bh864t.webp"
+            alt="HeroImg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default HeroThumbs;
